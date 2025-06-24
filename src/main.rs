8928cd0b39ed30aa3a1b25b92ddc297419028d16
Matheus Soares <69659304+msoareseq@users.noreply.github.com>
2025-06-23 23:30:01 -0300

    Create basic argument handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use std::env;
+use std::num::ParseIntError;
+
 fn main() {
-    println!("Hello, world!");
+    
+    // Argument handling
+    let args: Vec<String> = env::args().collect();
+    
+    // Instructions
+    let usage = r#"Usage: 
+        wordcounter [n] <file1> <file2> ...
+        
+        Parameters:
+            [n] - number of top n words to print (optional)
+            <file> - files to read
+    
+    "#;
+    
+    
+    if args.len() <= 1 {
+        println!("{}", usage);
+        return;
+    }
+    
+    let mut show_top_n_words = false;
+    let mut files: Vec<String> = vec![];
+    
+    let mut n_words = 10;
+    
+    match args[1].parse::<i32>() {
+        Ok(n) => {
+            n_words = n;
+            show_top_n_words = true;
+        }
+        Err(_) => {
+            println!("{}", "Top-n not defined. Showing top-10 words.");
+        }
+    }
+    
+    if show_top_n_words {
+        files = args[2..].to_vec();
+    } else { 
+        files = args[1..].to_vec();
+    }
+    
 }
